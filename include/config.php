8825ee8e3ac0823b0169ff51b7a46593764912ee
fdriteh <fd...@riteh.hr>
2017-06-09 03:09:36 +0000

    remove unused

--- include/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-	return array (
-			'db_server' => '127.0.0.1',
-			'db_user' => 'autotrolej',
-			'db_password' => '',
-			'db_database' => 'autotrolej'
-			);
-?>
--- page/logout.php
+++
@@ -1,4 +0,0 @@
-<?php
-	user_logout ();
-	redirect ('/');
-?>
